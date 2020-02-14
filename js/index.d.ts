066f44ca3ecfe14d3eebdf6bf7178c2743dadd20
ackerapple <acker.dawn.apple@gmail.com>
2020-02-14 16:55:27 -0500

    typings support

---
+++ js/index.d.ts
@@ -0,0 +1,13 @@
+export default queryBind;
+interface INameValuePairs {
+    [index: string]: boolean | string | number;
+}
+interface Db2ParameterizedSql {
+    sql: string;
+    parameters: (boolean | string | number)[];
+    valuesObject: any;
+}
+export declare function queryBind(string: string, nameValuePairs: INameValuePairs): Db2ParameterizedSql;
+export declare function queryBindToString(string: string, nameValuePairs: INameValuePairs, { quoteEscaper }?: {
+    quoteEscaper: string;
+}): string;
